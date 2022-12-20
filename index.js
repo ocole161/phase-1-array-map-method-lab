b915942267cb0cd28bfb8d53d1ae8635b6581910
Owen Cole <ocole161@gmail.com>
2022-12-19 23:25:07 -0700

    commit tree dcf04a1b137341fc20da263c503f1ffdc039b99d

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// function properCase(array) {
+//   lowerArray = array.map(string => {string.toLowerCase()})
+//   const properArray = []
+//   for (const string of lowerArray) {
+//     const stringSplit = string.split(" ")
+//       for (let i = 0; i < stringSplit.length; i++) {
+//         stringSplit[i] = stringSplit[i][0].toUpperCase() + stringSplit[i].slice(1)
+//         properString = stringSplit.join("")
+//         properArray.push(properString)
+//       }
+//   }
+//   console.log(properArray)
+//   return properArray
+// }
+
+// properCase(tutorials)
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(string) {
+    const words = string.split(" ")
+    const capWords = words.map(function(words) {
+      return words.charAt(0).toUpperCase() + words.slice(1)
+    }
+    )
+    const properString = capWords.join(" ")
+    return properString
+  })
 }
+
+console.log(titleCased())
